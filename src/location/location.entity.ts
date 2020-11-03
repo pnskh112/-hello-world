5330f365ddcc9e7310904d4551b60ff0f04715ef
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2020-11-03 07:45:29 +0000

    add:src/location/location.entity.ts

---
+++ src/location/location.entity.ts
@@ -0,0 +1,11 @@
+import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
+
+@Entity()
+export class Location {
+
+  @PrimaryGeneratedColumn('uuid')
+  private id: string;
+
+  @Column()
+  private name: string;
+}

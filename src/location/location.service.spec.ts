940dc0bd7920b2a8ae4fb4fc342c7a8a9c824c22
vagrant <vagrant@localhost.localdomain>
2020-11-05 04:59:54 +0000

    upd:src/location/location.service.spec.ts

--- src/location/location.service.spec.ts
+++ src/location/location.service.spec.ts
@@ -1,19 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
+import { Repository } from 'typeorm';
+import { Location } from './location.entity';
+import * as sinon from 'sinon';
 
 describe('LocationService', () => {
   let service: LocationService;
+  let mockRepository: Repository<Location>;
+  let mock: sinon.SinonMock;
 
   beforeEach(async () => {
-     service = new LocationService();
+     mockRepository = { } as Repository<Location>;
+     mockRepository = Object.assign(mockRepository, {
+       find: new function(),
+     });
+     mock = sinon.mock(mockRepository);
+     mock.expects('find').resolvers([
+     
+     ]);
+
+     service = new LocationService(mockRepository);
   });
 
   it('should return locations', () => {
-    expect(service.list()).toStrictEqual([
-      "Location 1",
-      "Location 2",
-      "Location 3",
+    expect(service.list()).resolves.toMatchObject([
     ]);
   });
 });
